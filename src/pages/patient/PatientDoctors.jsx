e70917ddbd843dd1bfb2bc4be42ce791cffc553d
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-06-27 00:00:12 +0700

    Update PatientDoctors.jsx

--- src/pages/patient/PatientDoctors.jsx
+++ src/pages/patient/PatientDoctors.jsx
@@ -10,11 +10,6 @@ export default function PatientDoctors({ doctors }) {
 
 			<div className="container">
 				<h1>Our Professional Team</h1>
-// 				<div className="d-flex justify-content-center position-relative">
-// 					<input type="text" className="patientDoctorsInput form-control position-absolut m-0 rounded-4" />
-// 					<i className="bi bi-search" />
-// 				</div>
-
 				<div className="doctorsTeam d-flex justify-content-between flex-wrap">
 					<Doctor doctors={doctors} />
 				</div>
