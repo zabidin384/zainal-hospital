f367cbf300574d31eec631883435658a17356323
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-06-28 12:59:11 +0700

    Update PatientAppointment.jsx

--- src/pages/patient/PatientAppointment.jsx
+++ src/pages/patient/PatientAppointment.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../../config";
 
 export default function patientAppointment({ doctors, token }) {
-	const [doctorId, setDoctorId] = useState(24);
+	const [doctorId, setDoctorId] = useState("");
 	const [date, setDate] = useState("");
 	const [description, setDescription] = useState("");
 	const [error, setError] = useState("");
@@ -59,7 +59,7 @@ export default function patientAppointment({ doctors, token }) {
 						{doctors.content ? (
 							doctors.content.map((doctor) => (
 								<option value={doctor.id} key={doctor.id}>
-									{doctor.firstName} {doctor.lastName}
+									doctorId && {doctor.firstName} {doctor.lastName}
 								</option>
 							))
 						) : (
