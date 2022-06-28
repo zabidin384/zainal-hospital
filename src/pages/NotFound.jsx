f1a45d8560eb426b035510fc6a7d648b311f662c
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-06-28 09:54:39 +0700

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+	const navigate = useNavigate();
+
 	return (
 		<div className="notFound text-center container top">
 			<img src="assets/404.jpg" alt="" />
 			<br />
-			<Link to="/">
-				<button className="mt-3">Back to Homepage</button>
-			</Link>
+			<div className="d-flex gap-3 justify-content-center">
+				<button className="mt-3" onClick={() => navigate(-1)}>
+					Back
+				</button>
+				<Link to="/">
+					<button className="mt-3">Homepage</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
