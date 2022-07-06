52f083f4150dcbdc63336ef8c636a0868d659829
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-07-06 11:23:46 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,20 +1,49 @@
-export const posts = [
-	{
-		id: 1,
-		title: "Article 1",
-		img: "assets/general/disease.png",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+export const posts = {
+	content: [
+		{
+			id: 1,
+			doctorId: 24,
+			title: "Article 1",
+			category: "Category1",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			publishDate: "2022-07-01",
+			owner: {
+				email: "doctor1@gmail.com",
+			},
+		},
+		{
+			id: 2,
+			doctorId: 24,
+			title: "Article 2",
+			category: "Category2",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			publishDate: "2022-07-02",
+			owner: {
+				email: "doctor2@gmail.com",
+			},
+		},
+		{
+			id: 3,
+			doctorId: 12,
+			title: "Article 3",
+			category: "Category3",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			publishDate: "2022-07-03",
+			owner: {
+				email: "doctor3@gmail.com",
+			},
+		},
+	],
+};
+
+export const post = {
+	id: "1",
+	title: "Article Details",
+	publishDate: "2022-07-01",
+	description:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. In similique delectus impedit, corrupti possimus fugit a, ad, ratione est incidunt rerum alias reprehenderit? Dolorem magnam harum dolores incidunt voluptatum debitis vitae, illo pariatur atque expedita, minima iusto iure earum a, nesciunt asperiores ad deleniti. Voluptatum, tempore? Illum eligendi iusto ut! Architecto repellat quasi deserunt consequuntur ipsa cumque sed laborum enim dicta quae eius aperiam reiciendis expedita possimus ex iure, quis fuga delectus aliquid est aliquam quia officiis nulla? Nam harum placeat fugit. Culpa in illo rerum voluptatem ea sequi? Vitae dolorem minima tenetur soluta illo, placeat molestiae fuga enim dolore!",
+	category: "CategoryDetails",
+	owner: {
+		email: "doctor@gmail.com",
 	},
-	{
-		id: 2,
-		title: "Article 2",
-		img: "assets/general/disease.png",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-	},
-	{
-		id: 3,
-		title: "Article 3",
-		img: "assets/general/disease.png",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-	},
-];
+};
